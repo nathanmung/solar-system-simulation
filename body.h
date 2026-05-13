e0b3b93d74134aeb1e3103261dcbe9db719badf6
Nathan Mung <mung.nathan@gmail.com>
2026-05-14 00:04:09 +0200

    creating the struct for a Body, that is any cosmic body in the solar system like the Sun, a planet, a moon etc...

---
+++ body.h
@@ -0,0 +1,16 @@
+#ifndef BODY_H
+#define BODY_H
+#include "vectors3.h"
+
+typedef struct body{
+
+    char name[100];
+    double mass;
+    double radius;
+    Vectors3 position;
+    Vectors3 speed;
+    Vectors3 acceleration;
+
+} Body ;
+
+#endif
