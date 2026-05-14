cf6b449e32bde3f0f9199e9c83f94352a06ae907
Nathan Mung <mung.nathan@gmail.com>
2026-05-14 15:28:02 +0200

    adding constants and its units

---
+++ constants.h
@@ -0,0 +1,11 @@
+#ifndef CONSTANTS_H
+#define CONSTANTS_H
+
+#define SOLAR_MASS 1.9884e30 // kg
+#define EARTH_MASS 5.972e24 // kg
+#define MOON_MASS  7.346e22 // kg
+#define UA 1.496e11 // m
+#define G_GRAVITATIONAL_CONSTANT 6.6743e-11 // m3.kg−1.s−2
+#define G_FORCE 9.81 // m.s-2
+
+#endif
