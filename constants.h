1342843e32c7f28275855c7feadd2d295c24431e
Nathan Mung <mung.nathan@gmail.com>
2026-05-14 15:46:43 +0200

    more constants

--- constants.h
+++ constants.h
@@ -4,7 +4,10 @@
 #define SOLAR_MASS 1.9884e30 // kg
 #define EARTH_MASS 5.972e24 // kg
 #define MOON_MASS  7.346e22 // kg
-#define UA 1.496e11 // m
+#define SUN_RADIUS 6.9634e8 // m
+#define EARTH_RADIUS 6.371e6 // m
+#define MOON_RADIUS 1.7374e6 // m
+#define AU 1.496e11 // m 
 #define G_GRAVITATIONAL_CONSTANT 6.6743e-11 // m3.kg−1.s−2
 #define G_FORCE 9.81 // m.s-2
 
